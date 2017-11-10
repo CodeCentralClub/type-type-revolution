61a7bfaf0199948487541879d0653ccb016b5af0
Willie Chalmers III <willieechalmers@gmail.com>
2017-11-10 15:59:33 -0600

    Remove useless attribute

--- src/org/codecentral/type/objects/Row.java
+++ src/org/codecentral/type/objects/Row.java
@@ -1,19 +1,12 @@
 package org.codecentral.type.objects;
 
 import java.awt.Graphics;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Row extends GameObject {
 
     private static final int HEIGHT = 128;
 
     private static final int DEFAULT_WIDTH = 1000;
-
-    /**
-     * A map of letters to row positions.
-     */
-    private Map<Letter, Integer> letters = new HashMap<>();
 
     public Row(int x, int y) {
         super(x, y);
