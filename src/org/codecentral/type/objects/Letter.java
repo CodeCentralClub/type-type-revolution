24e53aee2d2aa5c0c27d2056613a92c6415429df
Willie Chalmers III <willieechalmers@gmail.com>
2017-11-10 17:29:43 -0600

    Finalize Letter and Row class
    - The Row class still requires work to properly draw the letters

--- src/org/codecentral/type/objects/Letter.java
+++ src/org/codecentral/type/objects/Letter.java
@@ -1,10 +1,24 @@
 package org.codecentral.type.objects;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Letter extends GameObject {
 
+    /**
+     * The number of pixels to move upon each movement.
+     *
+     * @see #moveForward()
+     */
+    private int speed = 10;
+
     private char letter;
+
+    public Letter(int x, int y) {
+        this(generateRandomLetter(), x, y);
+    }
 
     public Letter(char letter, int x, int y) {
         super(x, y);
@@ -13,6 +27,24 @@ public class Letter extends GameObject {
 
     @Override
     public void onDraw(Graphics g) {
+        g.setFont(new Font("SansSerif", Font.BOLD, 10));
+        g.setColor(Color.WHITE);
         g.drawString(String.valueOf(letter), getX(), getY());
+    }
+
+    public void moveForward() {
+        setY(getY() - speed);
+    }
+
+    public void setSpeed(int newSpeed) {
+        speed = newSpeed;
+    }
+
+    private static char generateRandomLetter() {
+        int upperCase = ThreadLocalRandom.current().nextInt(56, 90 + 1);
+        int lowerCase = ThreadLocalRandom.current().nextInt(97, 122 + 1);
+        return (char) (ThreadLocalRandom.current().nextInt(0, 1 + 1) == 0
+                ? upperCase
+                : lowerCase);
     }
 }
--- src/org/codecentral/type/objects/Row.java
+++ src/org/codecentral/type/objects/Row.java
@@ -1,5 +1,6 @@
 package org.codecentral.type.objects;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,22 +11,27 @@ public class Row extends GameObject {
 
     private static final int DEFAULT_WIDTH = 1000;
 
-    List<Letter> letter = new ArrayList<>();
+    private List<Letter> letters = new ArrayList<>();
 
     public Row(int x, int y) {
         super(x, y);
     }
 
-    public void run() {
-
-    }
-
-    public void drawLetters() {
-
+    public void drawLetters(Graphics graphics) {
+        letters.forEach(letter -> letter.onDraw(graphics));
     }
 
     @Override
     public void onDraw(Graphics g) {
+        if (letters.size() == 0) {
+            for (int i = 0; i < 10; i++) {
+                letters.add(new Letter(200, 200));
+            }
+        }
+        // Draw this row
+        g.setColor(Color.BLACK);
         g.drawRect(getX(), getY(), HEIGHT, DEFAULT_WIDTH);
+        // Draw the letters
+        drawLetters(g);
     }
 }
